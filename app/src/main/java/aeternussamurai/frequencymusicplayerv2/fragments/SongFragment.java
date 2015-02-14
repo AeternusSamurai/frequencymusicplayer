2b020b57fb95ba3b46ea0eb5907e935ae07bb53d
AeturnusSamurai <darksamurai515@gmail.com>
2015-02-13 21:58:26 -0500

    First commit of project files

---
+++ app/src/main/java/aeternussamurai/frequencymusicplayerv2/fragments/SongFragment.java
@@ -0,0 +1,17 @@
+package aeternussamurai.frequencymusicplayerv2.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Chase on 2/10/2015.
+ */
+public class SongFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
